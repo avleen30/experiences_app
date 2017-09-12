7acf9833911b26d008a708339c15c0a8c1301f77
Avleen <avleen3@gmail.com>
2017-09-12 01:16:58 +0000

    removed avatars for now

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
     belongs_to :user, foreign_key: :creator_id
     belongs_to :category
 
-    mount_uploader :attachment, AvatarUploader
+    mount_uploader :avatar, AvatarUploader
 
 
     has_many :reviews
@@ -14,7 +14,7 @@ class Event < ApplicationRecord
     validates :name, presence: true
     validates :description, presence: true
     # validates :creator_id, presence: true
-    validates :start_date, presence: true
+    # validates :start_date, presence: true
     validates :end_date, presence: true
     validates :lng, presence: true
     validates :lat, presence: true

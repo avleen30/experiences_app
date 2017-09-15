a5361fc12170881d09da4441a8e0020511e06e96
Sean <leoseanquilty@gmail.com>
2017-09-15 17:15:40 +0000

    removed duplicated category

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,11 +40,10 @@ cat7 = Category.find_or_create_by! name: 'Meal'
 cat8 = Category.find_or_create_by! name: 'Photography'
 cat9 = Category.find_or_create_by! name: 'Art'
 cat10 = Category.find_or_create_by! name: 'Cultural'
-cat11 = Category.find_or_create_by! name: 'Sports'
-cat12 = Category.find_or_create_by! name: 'Cooking'
-cat13 = Category.find_or_create_by! name: 'Games'
-cat14 = Category.find_or_create_by! name: 'Shopping'
-cat15 = Category.find_or_create_by! name: 'Others'
+cat11 = Category.find_or_create_by! name: 'Cooking'
+cat12 = Category.find_or_create_by! name: 'Games'
+cat13 = Category.find_or_create_by! name: 'Shopping'
+cat14 = Category.find_or_create_by! name: 'Others'
 
 puts "Clearing old data...."
 
@@ -233,6 +232,151 @@ event10 = cat10.events.create!({
   lng: -79.386078,
   creator_id: User.all.shuffle.sample.id
 })
+
+event11 = cat11.events.create!({
+  name: "Cooking class",
+  description: "Let's try the free cooking class @ Longos!",
+  cover_img: open_asset('cooking1.jpg'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(0800..2359),
+  lat: 43.642459,
+  lng: -79.380840,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event12 = cat12.events.create!({
+  name: "Games & beers?",
+  description: "Wanna go see what the rec room is all about",
+  cover_img: open_asset('games1.jpg'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(1100..2359),
+  lat: 43.641194,
+  lng: -79.386770,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event13 = cat13.events.create!({
+  name: "Shopping for shoes!",
+  description: "Let's go shoe shopping at the Eaton Centre!",
+  cover_img: open_asset('shopping1.jpg'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(0800..2359),
+  lat: 43.654011,
+  lng: -79.380574,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event14 = cat14.events.create!({
+  name: "Let's aimlessly roam",
+  description: "Anyone want to just roam around or hang on a park bench?",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(0000..2359),
+  lat: 43.640694,
+  lng: -79.386078,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event15 = cat15.events.create!({
+  name: "Cultural",
+  description: "Want to go see the Toronto Necropolis?",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(0800..2359),
+  lat: 43.667657,
+  lng: -79.361570,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event16 = cat1.events.create!({
+  name: "Hiking through Riverdale",
+  description: "Let's go explore Riverdale!",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(0000..2359),
+  lat: 43.670367,
+  lng: -79.356343,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event17 = cat1.events.create!({
+  name: "Hike @ the park!",
+  description: "Let's checkout Tommy Thompson Park!",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(0000..2359),
+  lat: 43.629181,
+  lng: -79.327854,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event18 = cat2.events.create!({
+  name: "Quick pint?!",
+  description: "Anyone want to grab a quick pint at the Irish Embassy?!",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(1100..2359),
+  lat: 43.647967,
+  lng: -79.377248,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event19 = cat2.events.create!({
+  name: "Get sloppy at Sneaky's",
+  description: "You know the drill....",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(1100..2359),
+  lat: 43.656304,
+  lng: -79.407481,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event20 = cat3.events.create!({
+  name: "Free Party!",
+  description: "Open access to party @ Nathan Phillips",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(1100..2359),
+  lat: 43.652374,
+  lng: -79.383553,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event21 = cat3.events.create!({
+  name: "Music and dancing!",
+  description: "Let's go boogie-down at the Phoenix",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(1100..2359),
+  lat: 43.664664,
+  lng: -79.373971,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event22 = cat4.events.create!({
+  name: "Cup of tea",
+  description: "Anyone want to checkout Dineen? I've heard good things...",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(0000..2359),
+  lat: 43.651141,
+  lng: -79.378927,
+  creator_id: User.all.shuffle.sample.id
+})
+
+event23 = cat4.events.create!({
+  name: "Best coffee in town",
+  description: "Come try some and tell me I'm wrong",
+  cover_img: open_asset('xxxxxx'),
+  start_date: Date.new(2017,rand(8..12),rand(1..30)),
+  time: rand(0000..2359),
+  lat: 43.651249,
+  lng: -79.384229,
+  creator_id: User.all.shuffle.sample.id
+})
+
+
 
 ##events_users
 

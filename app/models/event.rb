7f609ad5057d3cd53d08b013a6a4ce1806dfcf9a
Sean <leoseanquilty@gmail.com>
2017-09-16 00:12:27 +0000

    fixed validation for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,8 +14,7 @@ class Event < ApplicationRecord
     validates :name, presence: true
     validates :description, presence: true
     validates :creator_id, presence: true
-    # validates :start_date, presence: true
-    validates :end_date, presence: true
+    validates :start_date, presence: true
     validates :lng, presence: true
     validates :lat, presence: true
 
